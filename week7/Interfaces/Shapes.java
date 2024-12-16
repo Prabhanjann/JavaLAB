228b9019c6c404d2704ef2c927c5d4375936d561
Prabhanjann <bprabhanjan.cs23@bmsce.ac.in>
2024-12-16 22:37:38 +0530

    Update Shapes.java

--- week7/Interfaces/Shapes.java
+++ week7/Interfaces/Shapes.java
@@ -1,54 +1,54 @@
 interface Shapes{
-default public void getperi(int n, int l){
-System.out.println("Perimeter of the regular polygon having " + n + " sides is "+(n*l));
-}
-public void getarea();
+  default public void getperi(int n, int l){
+    System.out.println("Perimeter of the regular polygon having " + n + " sides is "+(n*l));
+  }
+  public void getarea();
 }
 class Rect implements Shapes{
-private int a;
-private int b;
-public Rect(int a, int b){
-this.a=a;
-this.b=b;
-}
-@Override
-public void getarea(){
-System.out.println("The area is "+(a*b));
-}
+  private int a;
+  private int b;
+  public Rect(int a, int b){
+  this.a=a;
+  this.b=b;
+  }
+  @Override
+  public void getarea(){
+  System.out.println("The area is "+(a*b));
+  }
 }
 class Square implements Shapes{
-private int a;
-private int b;
-public Square(int a, int b){
-this.a=a;
-this.b=b;
-}
-@Override
-public void getarea(){
-System.out.println("The area is "+(a*b));
-}
+  private int a;
+  private int b;
+  public Square(int a, int b){
+    this.a=a;
+    this.b=b;
+  }
+  @Override
+  public void getarea(){
+    System.out.println("The area is "+(a*b));
+  }
 }
 class Triangle implements Shapes{
-private double l,b;
-public Triangle(int l, int b){
-this.l=l;
-this.b=b;
-}
-@Override
-public void getarea(){
-System.out.println("The area is "+(0.5*l*b));
-}
+  private double l,b;
+  public Triangle(int l, int b){
+  this.l=l;
+  this.b=b;
+  }
+  @Override
+  public void getarea(){
+    System.out.println("The area is "+(0.5*l*b));
+  }
 }
 class Run{
-public static void main(String args[]){
-Shapes sq= new Square(4 ,4);
-sq.getperi(4, 4);
-sq.getarea();
-Triangle tri= new Triangle(3, 6);
-tri.getperi(3, 6);
-tri.getarea();
-Rect rectangle= new Rect(4,8);
-rectangle.getperi(4, 8);
-rectangle.getarea();
-}
+  public static void main(String args[]){
+    Shapes sq= new Square(4 ,4);
+    sq.getperi(4, 4);
+    sq.getarea();
+    Triangle tri= new Triangle(3, 6);
+    tri.getperi(3, 6);
+    tri.getarea();
+    Rect rectangle= new Rect(4,8);
+    rectangle.getperi(4, 8);
+    rectangle.getarea();
+  }
 }
