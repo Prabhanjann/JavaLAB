207ae6d99fa3756be35d671845f85f6bc6e86f04
Prabhanjann <bprabhanjan.cs23@bmsce.ac.in>
2024-12-16 17:08:57 +0530

    Update Student.java

--- week2/Student.java
+++ week2/Student.java
@@ -1,79 +1,67 @@
 import java.util.Scanner;
-class Studdetails{
-	
-	int marks[]=new int[8];
-	int cred[]= new int[8];
-	String name,usn;
-	double sgpa;
-	Scanner sc =new Scanner(System.in);
 
+public class Student {
+    private String name;
+    private String usn;
+    private int[] credits;
+    private int[] marks;
+    private int n;
 
-	void getdetails(){
-		System.out.println(" Enter the USN ");
-		usn=sc.next();
-		System.out.println(" Enter the name ");
-		name=sc.next();
-		for(int i=0;i<8;i++){
-			System.out.println(" Enter the mark ");
-			marks[i]=sc.nextInt();
-			System.out.println(" Enter the credit ");
-			cred[i]=sc.nextInt();
-}
-}
+    public void acceptDetails() {
+        Scanner student = new Scanner(System.in);
 
-void display(){
+        System.out.println("Enter your name:");
+        name = student.nextLine(); 
 
-	
+        System.out.println("Enter USN:");
+        usn = student.nextLine();
 
-	System.out.println("usn"+" "+usn);
-	System.out.println("name"+" "+name);
-		
-	
-	System.out.println("SGPA is"+this.sgpa);
+        System.out.println("Enter no of subjects:");
+        n = student.nextInt();
 
+        credits = new int[n];
+        marks = new int[n];
 
-}
+        System.out.println("Enter no of credits in each sub:");
+        for (int i = 0; i < n; i++) {
+            credits[i] = student.nextInt();
+        }
 
-void calcSGPA(){
-	int sum=0;
-	int grdpoint=0;
+        System.out.println("Enter no of marks in each sub:");
+        for (int i = 0; i < n; i++) {
+            marks[i] = student.nextInt();
+        }
 
-	int x; 
-	for(int i=0;i<8;i++){
-		
-		
-		
-		x=marks[i]/10;
-		switch(x){
+        student.close();
+    }
 
-			case 10: grdpoint=10;break;
-			case 9:
-			case 8:
-			case 7:
-			case 6:
-			case 5:
-			case 4: grdpoint=x+1;break;
-			case 3:
-			case 2:
-			case 1:System.out.println("Fail");break;
-			}
-		sum=sum+ (grdpoint*cred[i]);		
-}
-	System.out.println(sum);
-	this.sgpa=sum/20.0;
+    public void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("USN: " + usn);
+        System.out.println("Credits and Marks:");
 
-}
-}
+        for (int i = 0; i < n; i++) {
+            System.out.println(credits[i] + " " + marks[i]);
+        }
+    }
 
+    public void calcSGPA() {
+        int total = 0;
+        int cred = 0;
 
-class Student{
-	public static void main(String args[]){
-		Studdetails s1=new Studdetails();
-		System.out.println("*********************************************\nName: B Prabhanjan\nUSN: 1BM23CS060\n*********************************************");
-		System.out.println("Enter the details of Student");
-		s1.getdetails();
-		s1.calcSGPA();
-		s1.display();
-	}
+        for (int i = 0; i < n; i++) {
+            total += credits[i] * marks[i];
+            cred += credits[i];
+        }
 
+        double sgpa = (double) total / cred;
+        System.out.println("SGPA: " + sgpa);
+    }
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.acceptDetails();
+        student.displayDetails();
+        student.calcSGPA();
+    }
 }
